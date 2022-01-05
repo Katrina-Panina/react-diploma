ba7ba824185779a85ebcfbbb8d425ff28313e3a7
Katrina <74234305+Katrina-Panina@users.noreply.github.com>
2022-01-05 12:07:58 +0300

    fix

--- src/components/CatalogContainer/index.js
+++
@@ -1,8 +0,0 @@
-export default function CatalogContainer(props) {
-  return (
-    <section className="catalog">
-      <h2 className="text-center">Каталог</h2>
-      {props.children}
-    </section>
-  );
-}
--- src/pages/CatalogId.js
+++
@@ -1,5 +0,0 @@
-import ItemFull from "../components/ItemFull";
-
-export default function CatalogId({ match }) {
-  return <ItemFull id={Number(match.params.id)} />;
-}
